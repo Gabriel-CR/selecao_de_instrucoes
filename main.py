0afc327d09f76d88c73ab61a048cc6eebccf5809
Gabriel Castro <gabriel.cgcr.cgcr@gmail.com>
2023-06-29 18:41:17 -0300

    q2: ADD, MUL, SUB, DIV

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ if __name__ == '__main__':
     res = sl.select_instruction("+(+(CONST 1,CONST 2),CONST 5))")
     res = sl.select_instruction("+(+(CONST 1,CONST 2),-(+(CONST 3,CONST 4),CONST 5))")
     res = sl.select_instruction("+(+(CONST 1,CONST 2),-(+(CONST 3,CONST 4),+(CONST 5,CONST 6)))")
+    res = sl.select_instruction("+(*(CONST 1,CONST 2),-(/(CONST 4,CONST 5),CONST 3))")
 
 
 
--- q2.py
+++ q2.py
@@ -23,60 +23,47 @@ def select(root):
     # se for uma operação de + - * /
     if root.data in ['+', '-', '*', '/']:
         # ADDI
-        if root.data == '+':
-            if "CONST" in root.right.data:
-                aux = Node(root.data)
-                aux.right = root.right
-                aux.escolhido = True
-                # calcular o custo do lado left
-                if root.left is not None:
-                    l = select(root.left)
-                    aux.custo = ([ut.turn_linear(aux)] + l.custo[0], 1 + l.custo[1])
-                else:
-                    aux.custo = ([ut.turn_linear(aux)], 1)
-                return aux
-            if "CONST" in root.left.data:
-                aux = Node(root.data)
-                aux.left = root.left
-                aux.escolhido = True
-                # calcular custo do lado right
-                if root.right is not None:
-                    r = select(root.right)
-                    aux.custo = ([ut.turn_linear(aux)] + r.custo[0], 1 + r.custo[1])
-                else:
-                    aux.custo = ([ut.turn_linear(aux)], 1)
-                return aux
-
-            # vou escolher o nó atual e chamar para os seus filhos
-            # ADD
+        if root.data == '+' and "CONST" in root.right.data:
+            aux = Node(root.data)
+            aux.right = root.right
+            aux.escolhido = True
+            # calcular o custo do lado left
             if root.left is not None:
                 l = select(root.left)
-                root.custo = (root.custo[0] + l.custo[0], root.custo[1] + l.custo[1])
+                aux.custo = ([ut.turn_linear(aux)] + l.custo[0], 1 + l.custo[1])
+            else:
+                aux.custo = ([ut.turn_linear(aux)], 1)
+            return aux
+        if root.data == '+' and "CONST" in root.left.data:
+            aux = Node(root.data)
+            aux.left = root.left
+            aux.escolhido = True
+            # calcular custo do lado right
             if root.right is not None:
                 r = select(root.right)
-                root.custo = (root.custo[0] + r.custo[0], root.custo[1] + r.custo[1])
-            root.custo = ([root.data] + root.custo[0], root.custo[1] + 1)
-            return root
+                aux.custo = ([ut.turn_linear(aux)] + r.custo[0], 1 + r.custo[1])
+            else:
+                aux.custo = ([ut.turn_linear(aux)], 1)
+            return aux
         # SUBI
-        if root.data == '-':
-            if "CONST" in root.right.data:
-                aux = Node(root.data)
-                aux.right = root.right
-                aux.escolhido = True
-                # calcular o custo do lado l
-                if root.left is not None:
-                    l = select(root.left)
-                    aux.custo = ([ut.turn_linear(aux)] + l.custo[0], 1 + l.custo[1])
-                else:
-                    aux.custo = ([ut.turn_linear(aux)], 1)
-                return aux
-            # SUB
+        if root.data == '-'and "CONST" in root.right.data:
+            aux = Node(root.data)
+            aux.right = root.right
+            aux.escolhido = True
+            # calcular o custo do lado l
             if root.left is not None:
                 l = select(root.left)
-                root.custo = (root.custo[0] + l.custo[0], root.custo[1] + l.custo[1])
-            if root.right is not None:
-                r = select(root.right)
-                root.custo = (root.custo[0] + r.custo[0], root.custo[1] + r.custo[1])
-            root.custo = ([root.data] + root.custo[0], root.custo[1] + 1)
-            return root
+                aux.custo = ([ut.turn_linear(aux)] + l.custo[0], 1 + l.custo[1])
+            else:
+                aux.custo = ([ut.turn_linear(aux)], 1)
+            return aux
+        # ADD, MUL, SUB, DIV
+        if root.left is not None:
+            l = select(root.left)
+            root.custo = (root.custo[0] + l.custo[0], root.custo[1] + l.custo[1])
+        if root.right is not None:
+            r = select(root.right)
+            root.custo = (root.custo[0] + r.custo[0], root.custo[1] + r.custo[1])
+        root.custo = ([root.data] + root.custo[0], root.custo[1] + 1)
+        return root
 
